7d4ab05b0c84cebf90d4f299ea5b32daa2f43463
EliezerAmbion <eliezerambion@gmail.com>
2022-06-19 17:42:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-iddxe5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-iddxe5)
---
+++ index.ts
@@ -0,0 +1,31 @@
+import './style.css';
+
+import { of, map, Observable, from } from 'rxjs';
+
+// ------- of operator ---------
+// of(1, 2, 3).subscribe({
+//   next: (res) => console.log(res),
+//   error: (res) => console.log(res),
+//   complete: () => console.log('complete'),
+// });
+
+// ------- from operator ---------
+// from(['alice', 'ben', 'charlie']).subscribe({
+//   next: (res) => console.log(res),
+//   error: (res) => console.log(res),
+//   complete: () => console.log('completed'),
+// });
+
+// ------- from operator (converting a promise to an observable) ---------
+// const somePromise = new Promise((resolve, reject) => {
+//   resolve('resolved!');
+//   reject('Rejected!');
+// });
+
+// const observableFromPromise$ = from(somePromise);
+
+// observableFromPromise$.subscribe({
+//   next: (res) => console.log(res),
+//   error: (err) => console.log('Error: ', err),
+//   complete: () => console.log('Completed'),
+// });
