daa94b40ce60cf8001b4ba6d80cb4c971dfde10c
EliezerAmbion <eliezerambion@gmail.com>
2022-06-25 16:01:49 +0800

    add fromEvent

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import './style.css';
 
-import { of, map, Observable, from } from 'rxjs';
+import { of, map, Observable, from, fromEvent } from 'rxjs';
 
 // ------- of operator ---------
 // of(1, 2, 3).subscribe({
@@ -31,3 +31,36 @@ import { of, map, Observable, from } from 'rxjs';
 // });
 
 // ------- fromEvent operator ---------
+const fromEventBtn = document.querySelector('.fromEventBtn');
+
+// as the first parameter, you provide the event target which in this case is the fromEventBtn
+// as the seconde parameter, you provide the event type which in this case is the click event
+// there will be a typing issue because by default the type of emitted values is set to 'Event'
+// use <MouseEvent> type
+// const fromEventBtn$ = fromEvent<MouseEvent>(fromEventBtn, 'click').subscribe(
+//   (event) => console.log(event.type, event.x, event.y)
+// );
+
+// setTimeout(() => {
+//   console.log('Unscubscribe');
+//   fromEventBtn$.unsubscribe();
+// }, 5000);
+
+const btn$ = new Observable<MouseEvent>((subscriber) => {
+  const clickHandlerFn = (event) => {
+    console.log('Event callback leaked?');
+    subscriber.next(event);
+  };
+
+  fromEventBtn.addEventListener('click', clickHandlerFn);
+
+  // teardown logic
+  return () => {
+    fromEventBtn.removeEventListener('click', clickHandlerFn);
+  };
+}).subscribe((event) => console.log(event.type, event.x, event.y));
+
+setTimeout(() => {
+  console.log('Unsubscribe');
+  btn$.unsubscribe();
+}, 5000);
