36c4e14130adeb1707f50d27c24bba843fb9cdb3
EliezerAmbion <eliezerambion@gmail.com>
2022-07-02 14:58:08 +0800

    add combineLatest

--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ import {
   timer,
   interval,
   forkJoin,
+  combineLatest,
 } from 'rxjs';
 
 // ------- of operator ---------
@@ -160,3 +161,27 @@ import {
 //       `${nameAjax.response.first_name} is from ${nationAjax.response.capital} and likes to eat ${foodAjax.response.dish}.`
 //     )
 // );
+
+// ------- combineLatest operator ---------
+const temperatureInput = document.getElementById('temperature-input');
+const conversionDropdown = document.getElementById('conversion-dropdown');
+const resultText = document.getElementById('result-text');
+
+const temperatureInputEvent$ = fromEvent(temperatureInput, 'input');
+const conversionInputEvent$ = fromEvent(conversionDropdown, 'input');
+
+combineLatest([temperatureInputEvent$, conversionInputEvent$]).subscribe(
+  ([temperatureInputEvent, conversionInputEvent]) => {
+    const temperature = Number(temperatureInputEvent.target['value']);
+    const conversion = conversionInputEvent.target['value'];
+
+    let result: number;
+    if (conversion === 'f-to-c') {
+      result = ((temperature - 32) * 5) / 9;
+    } else if (conversion === 'c-to-f') {
+      result = (temperature * 9) / 5 + 32;
+    }
+
+    resultText.innerText = String(result);
+  }
+);
